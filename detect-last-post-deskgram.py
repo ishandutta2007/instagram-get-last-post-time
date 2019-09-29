6213453e2c6f98ca043192670f8e27d10ea34c13
Ishan Dutta <ishandutta2007@MacBook-Pro.local>
2019-09-29 16:37:38 +0530

    other

--- detect-last-post-deskgram.py
+++ detect-last-post-deskgram.py
@@ -14,11 +14,11 @@ import json
 import traceback
 
 _user_agents = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
 ]
 
-class InstagramScraper:
 
+class InstagramScraper:
     def __init__(self, user_agents=None, proxy=None):
         self.user_agents = user_agents
         self.proxy = proxy
@@ -30,11 +30,14 @@ class InstagramScraper:
 
     def __request_url(self, url):
         try:
-            response = requests.get(url, headers={'User-Agent': self.__random_agent()}, proxies={'http': self.proxy,
-                                                                                                 'https': self.proxy})
+            response = requests.get(
+                url,
+                headers={"User-Agent": self.__random_agent()},
+                proxies={"http": self.proxy, "https": self.proxy},
+            )
             response.raise_for_status()
         except requests.HTTPError:
-            raise requests.HTTPError('Received non 200 status code from Instagram')
+            raise requests.HTTPError("Received non 200 status code from Instagram")
         except requests.RequestException:
             raise requests.RequestException
         else:
@@ -42,10 +45,12 @@ class InstagramScraper:
 
     @staticmethod
     def extract_json_data(html):
-        soup = BeautifulSoup(html, 'html.parser')
-        body = soup.find('body')
-        script_tag = body.find('script')
-        raw_string = script_tag.text.strip().replace('window._sharedData =', '').replace(';', '')
+        soup = BeautifulSoup(html, "html.parser")
+        body = soup.find("body")
+        script_tag = body.find("script")
+        raw_string = (
+            script_tag.text.strip().replace("window._sharedData =", "").replace(";", "")
+        )
         return json.loads(raw_string)
 
     def profile_page_metrics(self, profile_url):
@@ -53,34 +58,111 @@ class InstagramScraper:
         try:
             response = self.__request_url(profile_url)
             json_data = self.extract_json_data(response)
-            with open('profile_page_metrics.json', 'w') as f:
+            with open("profile_page_metrics.json", "w") as f:
                 json.dump(json_data, f)
-            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']
+            metrics = json_data["entry_data"]["ProfilePage"][0]["graphql"]["user"]
         except Exception as e:
             raise e
         else:
             for key, value in metrics.items():
-                if key != 'edge_owner_to_timeline_media':
+                if key != "edge_owner_to_timeline_media":
                     if value and isinstance(value, dict):
-                        value = value['count']
+                        value = value["count"]
                         results[key] = value
                     elif value:
                         results[key] = value
         return results
 
     def profile_page_recent_posts_time(self, profile):
-        profile_url = 'https://www.deskgram.cc/{}/'.format(profile)
+        profile_url = "https://www.deskgram.cc/{}/".format(profile)
         results = []
         try:
             response = self.__request_url(profile_url)
-            soup = BeautifulSoup(response, 'html.parser')
-            body = soup.find('body')
-            time_elems = body.find_all('span', {"class": "time"})#, _class="time")
+            soup = BeautifulSoup(response, "html.parser")
+            body = soup.find("body")
+            time_elems = body.find_all("span", {"class": "time"})
             times = [t.text for t in time_elems]
             time_difference = datetime.now() - dateparser.parse(times[0])
-            print('{:<25s} posted {:>12s} ago'.format(profile, str(time_difference)))
+            print("{:<25s} posted {:>12s} ago".format(profile, str(time_difference)))
         except Exception as e:
             print("Trouble finding info for " + profile_url)
+
+    def get_min_likes_from_tag_page_media_raingrande(self, tag):
+        print("Checking tag: {}".format(tag))
+        tag_url = "https://raingrande.com/hashtag/{}/".format(tag)
+        tot_media_likes = 0
+        tot_top_likes = 0
+        top_likes_list = []
+        try:
+            response = self.__request_url(tag_url)
+            soup = BeautifulSoup(response, "html.parser")
+            body = soup.find("body")
+            # pp.pprint(body)
+            like_elems = body.find_all(
+                "span", {"class": "like-count"}
+            )  # , {"class": "explore-posts"})
+            # print(like_elems)
+            print("len(like_elems)", len(like_elems))
+            likes = [
+                int(
+                    float(
+                        like_elem.text.replace("\n", "")
+                        .replace("K", "000")
+                        .replace("M", "000000")
+                        .strip()
+                    )
+                )
+                for like_elem in like_elems
+            ]
+            likes.sort()
+            print(likes)
+        except requests.HTTPError as http_e:
+            print(http_e)
+        except Exception as e:
+            if "Lets skip" not in str(e):
+                print(e)
+                traceback.print_exc()
+                print("Trouble finding info for " + tag_url)
+        return None
+
+    def get_min_likes_from_tag_page_media_zapoos(self, tag):
+        print("Checking tag: {}".format(tag))
+        tag_url = "https://zoopps.com/tag/{}/".format(tag)
+        tot_media_likes = 0
+        tot_top_likes = 0
+        top_likes_list = []
+        try:
+            response = self.__request_url(tag_url)
+            soup = BeautifulSoup(response, "html.parser")
+            body = soup.find("body")
+            # pp.pprint(body)
+            like_elems = body.find_all(
+                "span", {"class": "like-count"}
+            )  # , {"class": "explore-posts"})
+            # print(like_elems)
+            print("len(like_elems)", len(like_elems))
+            likes = [
+                int(
+                    float(
+                        like_elem.text.replace("\n", "")
+                        .replace("K", "000")
+                        .replace("M", "000000")
+                        .strip()
+                    )
+                )
+                for like_elem in like_elems
+            ]
+            likes.sort()
+            print(likes)
+        except requests.HTTPError as http_e:
+            print(http_e)
+        except Exception as e:
+            if "Lets skip" not in str(e):
+                print(e)
+                traceback.print_exc()
+                print("Trouble finding info for " + tag_url)
+        return None
+
 
 def main():
     obj = InstagramScraper()
@@ -93,5 +175,11 @@ def main():
     for x in username_list:
         obj.profile_page_recent_posts_time(x)
 
-if __name__ == '__main__':
+    tags = ["virat", "anushka", "deepika"]
+    for tag in tags:
+        obj.get_min_likes_from_tag_page_media_raingrande(tag)
+        obj.get_min_likes_from_tag_page_media_zapoos(tag)
+
+
+if __name__ == "__main__":
     main()
--- detect-last-post.py
+++ detect-last-post.py
@@ -13,11 +13,11 @@ import json
 import traceback
 
 _user_agents = [
-    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36"
 ]
 
-class InstagramScraper:
 
+class InstagramScraper:
     def __init__(self, user_agents=None, proxy=None):
         self.user_agents = user_agents
         self.proxy = proxy
@@ -29,11 +29,14 @@ class InstagramScraper:
 
     def __request_url(self, url):
         try:
-            response = requests.get(url, headers={'User-Agent': self.__random_agent()}, proxies={'http': self.proxy,
-                                                                                                 'https': self.proxy})
+            response = requests.get(
+                url,
+                headers={"User-Agent": self.__random_agent()},
+                proxies={"http": self.proxy, "https": self.proxy},
+            )
             response.raise_for_status()
         except requests.HTTPError:
-            raise requests.HTTPError('Received non 200 status code from Instagram')
+            raise requests.HTTPError("Received non 200 status code from Instagram")
         except requests.RequestException:
             raise requests.RequestException
         else:
@@ -41,10 +44,12 @@ class InstagramScraper:
 
     @staticmethod
     def extract_json_data(html):
-        soup = BeautifulSoup(html, 'html.parser')
-        body = soup.find('body')
-        script_tag = body.find('script')
-        raw_string = script_tag.text.strip().replace('window._sharedData =', '').replace(';', '')
+        soup = BeautifulSoup(html, "html.parser")
+        body = soup.find("body")
+        script_tag = body.find("script")
+        raw_string = (
+            script_tag.text.strip().replace("window._sharedData =", "").replace(";", "")
+        )
         return json.loads(raw_string)
 
     def profile_page_metrics(self, profile_url):
@@ -52,46 +57,52 @@ class InstagramScraper:
         try:
             response = self.__request_url(profile_url)
             json_data = self.extract_json_data(response)
-            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']
+            metrics = json_data["entry_data"]["ProfilePage"][0]["graphql"]["user"]
         except Exception as e:
             raise e
         else:
             for key, value in metrics.items():
-                if key != 'edge_owner_to_timeline_media':
+                if key != "edge_owner_to_timeline_media":
                     if value and isinstance(value, dict):
-                        value = value['count']
+                        value = value["count"]
                         results[key] = value
                     elif value:
                         results[key] = value
         return results
 
     def profile_page_recent_posts_time(self, profile):
-        profile_url = 'https://www.instagram.com/{}/'.format(profile)
+        profile_url = "https://www.instagram.com/{}/".format(profile)
         results = []
         try:
             response = self.__request_url(profile_url)
             json_data = self.extract_json_data(response)
-            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']["edges"]
+            metrics = json_data["entry_data"]["ProfilePage"][0]["graphql"]["user"][
+                "edge_owner_to_timeline_media"
+            ]["edges"]
             for node in metrics:
-                node = node.get('node')
+                node = node.get("node")
                 if node and isinstance(node, dict):
                     results.append(node)
-            time_difference = datetime.now() - datetime.fromtimestamp(int(results[0]['taken_at_timestamp']))
-            print('{:<25s} posted {:>12s} ago'.format(profile, str(time_difference)))
-        
+            time_difference = datetime.now() - datetime.fromtimestamp(
+                int(results[0]["taken_at_timestamp"])
+            )
+            print("{:<25s} posted {:>12s} ago".format(profile, str(time_difference)))
+
         except:
             print("Trouble finding info for " + profile_url)
 
     def profile_page_last9_post_metrics(self, profile):
-        profile_url = 'https://www.instagram.com/{}/'.format(profile)
+        profile_url = "https://www.instagram.com/{}/".format(profile)
         # print(profile)
         try:
             response = self.__request_url(profile_url)
             json_data = self.extract_json_data(response)
-            metrics = json_data['entry_data']['ProfilePage'][0]['graphql']['user']['edge_owner_to_timeline_media']["edges"]
+            metrics = json_data["entry_data"]["ProfilePage"][0]["graphql"]["user"][
+                "edge_owner_to_timeline_media"
+            ]["edges"]
             likes = []
             for ctr, node in enumerate(metrics):
-                node = node.get('node')
+                node = node.get("node")
                 # print("{}.Comments: {}".format(ctr, node["edge_media_to_comment"]["count"]))
                 # print("{}.Likes: {}".format(ctr, node["edge_media_preview_like"]["count"]))
                 likes.append(node["edge_media_preview_like"]["count"])
@@ -109,8 +120,8 @@ class InstagramScraper:
         time = datetime.fromtimestamp(timestamp_taken)
         time_difference = datetime.now() - time
         print("Time: {}".format(time))
-        print('Posted {:>12s} ago'.format(str(time_difference)))
-        print('Posted {} secs ago'.format(str(timestamp_difference)))
+        print("Posted {:>12s} ago".format(str(time_difference)))
+        print("Posted {} secs ago".format(str(timestamp_difference)))
 
         print("Comments: {}".format(node["edge_media_to_comment"]["count"]))
         print("Likes: {}".format(node["edge_media_preview_like"]["count"]))
@@ -124,33 +135,90 @@ class InstagramScraper:
         try:
             response = self.__request_url(tag_url)
             json_data = self.extract_json_data(response)
-            with open('tags_{}.json'.format(tag), 'w') as f:
+            with open("tags_{}.json".format(tag), "w") as f:
                 json.dump(json_data, f)
-            total_medias = json_data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']["count"]
+            total_medias = json_data["entry_data"]["TagPage"][0]["graphql"]["hashtag"][
+                "edge_hashtag_to_media"
+            ]["count"]
             print("Total Medias: {}".format(total_medias))
-            medias = json_data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']["edges"]
+            medias = json_data["entry_data"]["TagPage"][0]["graphql"]["hashtag"][
+                "edge_hashtag_to_media"
+            ]["edges"]
             for ctr, node in enumerate(medias):
                 print("Media: {}".format(ctr))
-                node = node.get('node')
+                node = node.get("node")
                 self.print_node(node)
                 tot_media_likes += node["edge_media_preview_like"]["count"]
 
-            top_posts = json_data['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_top_posts']["edges"]
+            top_posts = json_data["entry_data"]["TagPage"][0]["graphql"]["hashtag"][
+                "edge_hashtag_to_top_posts"
+            ]["edges"]
             print("Total top Posts: {}".format(len(top_posts)))
             for ctr, node in enumerate(top_posts):
                 print("Top: {}".format(ctr))
-                node = node.get('node')
+                node = node.get("node")
                 self.print_node(node)
                 tot_top_likes += node["edge_media_preview_like"]["count"]
                 top_likes_list.append(node["edge_media_preview_like"]["count"])
-            print('tot_media_likes:', tot_media_likes)
-            print('tot_top_likes:', tot_top_likes)
-            print('top_likes_list:', top_likes_list)
+            print("tot_media_likes:", tot_media_likes)
+            print("tot_top_likes:", tot_top_likes)
+            print("top_likes_list:", top_likes_list)
             return min(top_likes_list)
         except Exception as e:
             print(e)
             traceback.print_exc()
             print("Trouble finding info for " + tag_url)
+
+    def get_min_likes_from_tag_page_media(self, tag):
+        print("Checking tag: {}".format(tag))
+        tag_url = "https://www.instagram.com/explore/tags/{}/".format(tag)
+        tot_media_likes = 0
+        tot_top_likes = 0
+        top_likes_list = []
+        try:
+            response = self.__request_url(tag_url)
+            json_data = self.extract_json_data(response)
+            # with open("tags_{}.json".format(tag), "w") as f:
+            #     json.dump(json_data, f)
+            total_medias = json_data["entry_data"]["TagPage"][0]["graphql"]["hashtag"][
+                "edge_hashtag_to_media"
+            ]["count"]
+            print("Total Medias: {}".format(total_medias))
+            medias = json_data["entry_data"]["TagPage"][0]["graphql"]["hashtag"][
+                "edge_hashtag_to_media"
+            ]["edges"]
+            print("len(medias)", len(medias))
+            for ctr, node in enumerate(medias):
+                # print("Media: {}".format(ctr))
+                node = node.get("node")
+                # self.print_node(node)
+                tot_media_likes += node["edge_media_preview_like"]["count"]
+
+            top_posts = json_data["entry_data"]["TagPage"][0]["graphql"]["hashtag"][
+                "edge_hashtag_to_top_posts"
+            ]["edges"]
+            print("Total top Posts: {}".format(len(top_posts)))
+            for ctr, node in enumerate(top_posts):
+                # print("Top: {}".format(ctr))
+                node = node.get("node")
+                # self.print_node(node)
+                tot_top_likes += node["edge_media_preview_like"]["count"]
+                top_likes_list.append(node["edge_media_preview_like"]["count"])
+            print("tot_media_likes({}): {}".format(len(medias), tot_media_likes))
+            print("tot_top_likes({}): {}".format(len(top_posts), tot_top_likes))
+            print("top_likes_list: {}".format(top_likes_list))
+            print("====xxx===")
+            if len(top_likes_list) > 0:
+                print("min(top_likes_list): {}".format(min(top_likes_list)))
+                return min(top_likes_list)
+        except requests.HTTPError as http_e:
+            print(http_e)
+        except Exception as e:
+            if "Lets skip" not in str(e):
+                print(e)
+                traceback.print_exc()
+                print("Trouble finding info for " + tag_url)
+        return None
 
 
 def main():
@@ -164,5 +232,10 @@ def main():
     for x in username_list:
         obj.profile_page_recent_posts_time(x)
 
-if __name__ == '__main__':
+    tags = ["virat", "anushka", "deepika"]
+    for tag in tags:
+        obj.get_min_likes_from_tag_page_media(tag)
+
+
+if __name__ == "__main__":
     main()
